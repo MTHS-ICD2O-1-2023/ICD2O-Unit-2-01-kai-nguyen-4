7c804b216c9a5bd55eb1af2aca0a6ebecb9271ea
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-03-05 19:27:33 +0000

    .

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: Mar 2024
 // This file contains the JS functions for index.html
 function myButtonClicked() {
   document.getElementById("answer").innerHTML = "<p>Hello, World!</p>"
